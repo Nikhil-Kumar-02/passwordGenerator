68f1fb778b54a9a5924d3344fac6858dfcd6a3b3
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2023-08-19 18:43:19 +0530

    now the password strength can be seen with the color on the basis of number of checkbox selected

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const symbolsCheck = document.querySelector("#symbols");
 const indicator = document.querySelector("[data-indicator]");
 const generateBtn = document.querySelector(".generateButton");
 const allCheckBox = document.querySelectorAll("input[type=checkbox]");
+const anyCheckBox = document.querySelector(".input-container");
 
 
 //below is the configuration when the page is reloaded
@@ -22,6 +23,7 @@ defaultSliderconfig();
 function defaultSliderconfig(){
     inputSlider.value = passwordLength;
     lengthDisplay.textContent = passwordLength;
+    uppercaseCheck.checked = 1;
 }
 
 function updateSliderValue(){
@@ -30,3 +32,65 @@ function updateSliderValue(){
 }
 inputSlider.addEventListener('input' , updateSliderValue);
 
+// anyCheckBox.addEventListener('click' , (event)=>{
+//     if(event.target.checked){
+//         checkCount++;
+//         console.log(checkCount);
+//     }
+//     else{
+//         checkCount--;
+//         console.log(checkCount);
+//     }
+// })
+uppercaseCheck.addEventListener('click' , (event)=>{
+    if(uppercaseCheck.checked){
+        checkCount++;
+        console.log(checkCount);
+    }
+    else{
+        checkCount--;
+        console.log(checkCount);
+    }
+})
+lowercaseCheck.addEventListener('click' , (event)=>{
+    if(lowercaseCheck.checked){
+        checkCount++;
+        console.log(checkCount);
+    }
+    else{
+        checkCount--;
+        console.log(checkCount);
+    }
+})
+numbersCheck.addEventListener('click' , (event)=>{
+    if(numbersCheck.checked){
+        checkCount++;
+        console.log(checkCount);
+    }
+    else{
+        checkCount--;
+        console.log(checkCount);
+    }
+})
+symbolsCheck.addEventListener('click' , (event)=>{
+    if(symbolsCheck.checked){
+        checkCount++;
+        console.log(checkCount);
+    }
+    else{
+        checkCount--;
+        console.log(checkCount);
+    }
+})
+
+generateBtn.addEventListener('click',function(){
+    if(checkCount <= 1){
+        indicator.style.backgroundColor="red";
+    }
+    else if(checkCount < 4){
+        indicator.style.backgroundColor="yellow";
+    }
+    else{
+        indicator.style.backgroundColor="green";
+    }
+})
